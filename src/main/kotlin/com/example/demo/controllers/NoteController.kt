c1892ea64c7c678916546fd7eda4ca77ff440712
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-04-25 16:10:17 +0900

    db connectivity and notes

---
+++ src/main/kotlin/com/example/demo/controllers/NoteController.kt
@@ -0,0 +1,65 @@
+package com.example.demo.controllers
+
+import com.example.demo.database.model.Note
+import com.example.demo.database.repository.NoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.*
+import java.time.Instant
+
+@RestController
+@RequestMapping("/notes")
+class NoteController(
+    private val repository: NoteRepository
+) {
+
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long,
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant
+    )
+
+    @PostMapping
+    fun save(
+        @RequestBody body: NoteRequest
+    ): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId()
+            )
+        )
+        return note.toResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = true) ownerId: String,
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.toResponse()
+        }
+    }
+}
+
+private fun Note.toResponse(): NoteController.NoteResponse {
+    return NoteController.NoteResponse(
+        id = id.toHexString(),
+        title = title,
+        content = content,
+        color = color,
+        createdAt = createdAt,
+    )
+}
